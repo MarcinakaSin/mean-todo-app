86dd14d615a2551a9c36146365c8a1a9b8fd4e59
Marcin aka Sin <marcinmu330@gmail.com>
2016-03-11 09:25:38 -0600

    updated db connection

--- src/database.js
+++ src/database.js
@@ -2,7 +2,15 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mean-todo', function(err) {
+/*mongoose.connect('mongodb://localhost/mean-todo', function(err) {
+	if(err) {
+		console.log('Failed connecting to Mongodb!');
+	} else {
+		console.log('Successfully connected to Mongodb!');
+	}
+});*/
+
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_ACCOUNT + '@ds011399.mlab.com:11399/heroku_gpnhf9gl', function(err) {
 	if(err) {
 		console.log('Failed connecting to Mongodb!');
 	} else {
