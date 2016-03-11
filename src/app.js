44048f739c8fc7a750d63def9009a3a6dc505fd1
Marcin aka Sin <marcinmu330@gmail.com>
2016-03-11 08:36:22 -0600

    added missing packages

--- src/app.js
+++ src/app.js
@@ -6,14 +6,17 @@ var router = require('./api');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 require('./database');
 require('./seed');
+
 
 app.use('/', express.static('public'));
 app.use(parser.json());
 
 app.use('/api', router);
 
-app.listen(3000, function(){
-	console.log("The server is running on port 3000!");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
