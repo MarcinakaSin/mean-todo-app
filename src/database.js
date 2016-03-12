510e39d5c23207064441dc2413d0298c2fcc9f23
Marcin aka Sin <marcinmu330@gmail.com>
2016-03-12 16:44:25 -0600

    updated database file

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect((process.env.MONGOLAB_URI || 'mongodb://localhost/mean-todo'), function(err) {
+mongoose.connect(process.env.MONGOLAB_URI, function(err) {
   if (err) {
     console.log('Failed connecting to MongoDB!');
   } else {
